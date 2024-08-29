3bd443e1a090ac46ef0cd66d2d206c25425c0124
Ethan <etsausa@gmail.com>
2024-08-29 16:02:35 -0400

    fix admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addWatchTarget("src/assets/");
     eleventyConfig.addPassthroughCopy("src/css/");
     eleventyConfig.addWatchTarget("src/css/");
-
+    eleventyConfig.addPassthroughCopy("src/admin");
 
     return {
         dir: {
